76f8d180819669fbea3c572db8d02ea661892f53
mohamedamr77 <moamr947@gmail.com>
2024-05-18 02:41:42 +0300

    inital commit

---
+++ lib/features/onboarding/views/onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomOnboarding extends StatelessWidget {
+  const CustomOnboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
