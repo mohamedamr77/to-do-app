42d5fe6dbda5b8bdf5e6aabe99b2a8e61827762c
mohamedamr77 <moamr947@gmail.com>
2024-05-18 05:46:59 +0300

    make container take photo

--- lib/features/regester/views/regester_screen.dart
+++ lib/features/regester/views/regester_screen.dart
@@ -10,7 +10,52 @@ class CustomRegesterScreen extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image(image: AssetImage(AppImage.regesterImage)),
+            Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+              Image(image: AssetImage(AppImage.regesterImage)),
+              Stack(
+                alignment: Alignment.center,
+                children:[
+                  Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                          colors: [
+                            Color(0xff74B3E3),
+                            Color(0xffCDADD2),
+                          ]
+                      )
+                  ),
+                ),
+                  Container(
+                    width: 118,
+                    height: 118,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ShaderMask(
+
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(colors: [
+                              Color(0xff76B3E3),
+                              Color(0xff68A2D0),
+                        ]).createShader(bounds);
+                      },
+                      child: Icon(Icons.add_a_photo,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                  )
+                ]
+              ),
+            ]
+            ),
+
           ],
         ),
     );
