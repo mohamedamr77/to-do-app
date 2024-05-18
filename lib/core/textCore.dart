8244b369f0cd72c27c758df9a9069011ba010621
mohamedamr77 <moamr947@gmail.com>
2024-05-18 12:19:14 +0300

    add enter your name

--- lib/core/textCore.dart
+++ lib/core/textCore.dart
@@ -3,5 +3,7 @@ class AppText{
   static const String  onboardingDescriptionText="This productive tool is designed to help you better manage your task project-wise conveniently!";
   static const String  startText="Let’s Start";
   static const String  addPhotoText="Add Photo";
+  static const String  yourNameText="Your Name";
+  static const String  enterNameText="Enter Your Name";
 
 }
--- lib/features/regester/views/regester_screen.dart
+++ lib/features/regester/views/regester_screen.dart
@@ -11,76 +11,133 @@ class CustomRegesterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-              Image(image: AssetImage(AppImage.regesterImage)),
-
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               Stack(
-                alignment: Alignment.center,
-                children:[
-                  Container(
-                  width: MediaQuery.of(context).size.width*0.3353,
-                  height: MediaQuery.of(context).size.height*0.16,
-                  //   width: 120,
-                  // height: 120,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                          colors: [
-                             AppColor.skyBlueColor,
-                             AppColor.lightPurpleColor,
-                          ]
-                      )
-                  ),
-                ),
-                  Container(
-                    width: MediaQuery.of(context).size.width*0.325,
-                    height: MediaQuery.of(context).size.height*0.156,
-                    // width: 118,
-                    // height: 118,
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                Image(image: AssetImage(AppImage.regesterImage)),
+          
+                Stack(
+                  alignment: Alignment.center,
+                  children:[
+                    Container(
+                    width: MediaQuery.of(context).size.width*0.3353,
+                    height: MediaQuery.of(context).size.height*0.16,
+                    //   width: 120,
+                    // height: 120,
                     decoration: BoxDecoration(
-                      color: AppColor.whiteColor,
-                      borderRadius: BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                            colors: [
+                               AppColor.skyBlueColor,
+                               AppColor.lightPurpleColor,
+                            ]
+                        )
                     ),
-                    child: ShaderMask(
-
-                      shaderCallback: (Rect bounds) {
-                        return LinearGradient(colors: [
-                              AppColor.skyBlueColor,
-                               AppColor.mediumBlueColor,
-                        ]
-                        ).createShader(bounds);
-                      },
-                      child: Icon(Icons.add_a_photo,
-                        color:  AppColor.whiteColor,
-                        size: 40,
+                  ),
+                    Container(
+                      width: MediaQuery.of(context).size.width*0.325,
+                      height: MediaQuery.of(context).size.height*0.156,
+                      // width: 118,
+                      // height: 118,
+                      decoration: BoxDecoration(
+                        color: AppColor.whiteColor,
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                    ),
-                  )
-                ]
+                      child: ShaderMask(
+          
+                        shaderCallback: (Rect bounds) {
+                          return LinearGradient(colors: [
+                                AppColor.skyBlueColor,
+                                 AppColor.mediumBlueColor,
+                          ]
+                          ).createShader(bounds);
+                        },
+                        child: Icon(Icons.add_a_photo,
+                          color:  AppColor.whiteColor,
+                          size: 40,
+                        ),
+                      ),
+                    )
+                  ]
+                ),
+          
+          
+              ]
               ),
-
-
-            ]
-            ),
-            SizedBox(height: 5,),
-            Text(AppText.addPhotoText,
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                color: AppColor.slateGrayColor,
-                fontFamily: "LexendDecaRegularStyle",
+              SizedBox(height: 5,),
+              Text(AppText.addPhotoText,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: AppColor.slateGrayColor,
+                  fontFamily: "LexendDecaRegularStyle",
+                ),
               ),
+          
+              Padding(
+                padding: const EdgeInsets.all(22.0),
+                child: Stack(
+                    alignment: Alignment.center,
+                    children:[
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height*0.154,
+                        //   width: 120,
+                        // height: 120,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                                colors: [
+                                  AppColor.skyBlueColor,
+                                  AppColor.lightPurpleColor,
+                                ]
+                            )
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.all(22),
+                        width: MediaQuery.of(context).size.width-50,
 
-            ),
-
-            CustomButtonStarted(),
-
-          ],
+                        // width: 118,
+                        // height: 118,
+                        decoration: BoxDecoration(
+                          color: AppColor.whiteColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(AppText.yourNameText,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: AppColor.slateGrayColor,
+                                fontFamily: "LexendDecaRegularStyle",
+                              ),
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                               hintText: AppText.enterNameText,
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ],
+                        ) ,
+                      ),
+                    ]
+                ),
+              ),
+          
+              CustomButtonStarted(),
+          
+            ],
+          ),
         ),
     );
   }
