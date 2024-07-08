38ecc50cbae05db393c8baf3dd87564da215b7ed
mohamedamr77 <moamr947@gmail.com>
2024-07-08 23:43:11 +0300

    make validator in regester Screen

--- lib/features/regester/views/regester_screen.dart
+++ lib/features/regester/views/regester_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:todo/core/colorCore.dart';
@@ -11,143 +10,149 @@ import '../../onboarding/views/widgets/button_Started.dart';
 
 class CustomRegesterScreen extends StatefulWidget {
   const CustomRegesterScreen({super.key});
-  static String id="Register Screen";
+  static String id = "Register Screen";
 
   @override
   State<CustomRegesterScreen> createState() => _CustomRegesterScreenState();
 }
 
 class _CustomRegesterScreenState extends State<CustomRegesterScreen> {
-  XFile ? myPhoto ;
+  XFile? myPhoto;
   final ImagePicker picker = ImagePicker();
-  Future<XFile?>   pickImage( ) async{
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+
+  Future<XFile?> pickImage() async {
     final XFile? image = await picker.pickImage(source: ImageSource.gallery);
     return image;
   }
 
-  TextEditingController nameController = TextEditingController();
+  void _submitForm() {
+    if (_formKey.currentState!.validate() && myPhoto != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePageScreen(
+            name: nameController.text,
+            photo: File(myPhoto!.path),
+          ),
+        ),
+      );
+    } else {
+      if (myPhoto == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please select a photo')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding:  EdgeInsets.symmetric(vertical:  MediaQuery.of(context).size.height*0.05),
-          child:  Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              Column(
-                children: [
-                  Stack(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height * 0.05),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Stack(
                       alignment: Alignment.bottomCenter,
                       children: [
-
                         const Image(image: AssetImage(ImageApp.regesterImage)),
-
                         Stack(
-                            alignment: Alignment.center,
-                            children:[
-                              Container(
-                                width: MediaQuery.of(context).size.width*0.3353,
-                                height: MediaQuery.of(context).size.height*0.16,
-                                //   width: 120,
-                                // height: 120,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    gradient: const LinearGradient(
-                                        colors: [
-                                          ColorApp.skyBlueColor,
-                                          ColorApp.lightPurpleColor,
-                                        ]
-                                    )
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.3353,
+                              height: MediaQuery.of(context).size.height * 0.16,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                gradient: const LinearGradient(
+                                  colors: [
+                                    ColorApp.skyBlueColor,
+                                    ColorApp.lightPurpleColor,
+                                  ],
                                 ),
                               ),
-
-                              GestureDetector(
-                                onTap: (){
-                                  pickImage().then((value){
-                                    myPhoto = value;
-                                    setState(() {});
-                                  });
-                                },
-                                child: Container(
-                                  width: MediaQuery.of(context).size.width*0.325, //width=128
-                                  height: MediaQuery.of(context).size.height*0.156, //height=118
-                                  decoration: BoxDecoration(
-                                    color: ColorApp.whiteColor,
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-
-                                  child: ShaderMask(
-                                    shaderCallback: (Rect bounds) {
-                                      return const LinearGradient(colors: [
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                pickImage().then((value) {
+                                  myPhoto = value;
+                                  setState(() {});
+                                });
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.325,
+                                height: MediaQuery.of(context).size.height * 0.156,
+                                decoration: BoxDecoration(
+                                  color: ColorApp.whiteColor,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: ShaderMask(
+                                  shaderCallback: (Rect bounds) {
+                                    return const LinearGradient(
+                                      colors: [
                                         ColorApp.skyBlueColor,
                                         ColorApp.mediumBlueColor,
-                                      ]
-                                      ).createShader(bounds);
-                                    },
-
-                                    child: myPhoto==null? const Icon(
-                                      Icons.add_a_photo,
-                                      color:  ColorApp.whiteColor,
-                                      size: 40,
-                                    ) : ClipRRect(
-                                      borderRadius: BorderRadius.circular(20),
-                                      child: Image.file(File(myPhoto!.path),
-                                        fit: BoxFit.fill,
-                                      ),
+                                      ],
+                                    ).createShader(bounds);
+                                  },
+                                  child: myPhoto == null
+                                      ? const Icon(
+                                    Icons.add_a_photo,
+                                    color: ColorApp.whiteColor,
+                                    size: 40,
+                                  )
+                                      : ClipRRect(
+                                    borderRadius: BorderRadius.circular(20),
+                                    child: Image.file(
+                                      File(myPhoto!.path),
+                                      fit: BoxFit.fill,
                                     ),
                                   ),
                                 ),
                               ),
-                            ]
+                            ),
+                          ],
                         ),
-
-
-                      ]
-                  ),
-
-                  const SizedBox(height: 5,),
-
-                  TextButton(
-                    onPressed: () {
-                      pickImage().then((value){
-                        myPhoto = value;
-                        setState(() {});
-                      });
-                    },
-                    child:  Text( myPhoto==null?
-                    AppText.addPhotoText:
-                    "Update photo"
-                      ,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: ColorApp.slateGrayColor,
-                        fontFamily: "LexendDecaRegularStyle",
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    TextButton(
+                      onPressed: () {
+                        pickImage().then((value) {
+                          myPhoto = value;
+                          setState(() {});
+                        });
+                      },
+                      child: Text(
+                        myPhoto == null ? AppText.addPhotoText : "Update photo",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: ColorApp.slateGrayColor,
+                          fontFamily: "LexendDecaRegularStyle",
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 5,),
-
-              EnterName(controller: nameController,),
-
-              CustomButtonStarted(text: 'Get Started ', trailing: null,
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>
-                      HomePageScreen (
-                        name: nameController.text,
-                        photo: File(myPhoto!.path),
-                      )
-                  )
-                  );
-                },
-              ),
-
-            ],
+                  ],
+                ),
+                const SizedBox(height: 5),
+                EnterName(controller: nameController),
+                CustomButtonStarted(
+                  text: 'Get Started ',
+                  trailing: null,
+                  onTap: _submitForm,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/regester/views/widgets/enter_name.dart
+++ lib/features/regester/views/widgets/enter_name.dart
@@ -3,71 +3,72 @@ import '../../../../core/colorCore.dart';
 import '../../../../core/textCore.dart';
 
 class EnterName extends StatelessWidget {
-  const EnterName({super.key,required this.controller});
- final TextEditingController? controller;
+  const EnterName({super.key, required this.controller});
+  final TextEditingController? controller;
+
   @override
   Widget build(BuildContext context) {
-    return   Padding(
+    return Padding(
       padding: const EdgeInsets.all(22.0),
       child: Stack(
-          alignment: Alignment.center,
-          children:[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height*0.154,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: const LinearGradient(
-                      colors: [
-                        ColorApp.skyBlueColor,
-                        ColorApp.lightPurpleColor,
-                      ]
-                  )
-              ),
-            ),
-
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: const EdgeInsets.all(22),
-              width: MediaQuery.of(context).size.width-50,
-              // width: 118,
-              // height: 118,
-              decoration: BoxDecoration(
-                color: ColorApp.whiteColor,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child:
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-
-                  const Text(AppText.yourNameText,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: ColorApp.slateGrayColor,
-                      fontFamily: "LexendDecaRegularStyle",
-                    ),
-                  ),
-
-                  TextFormField(
-                    controller:controller ,
-                    decoration: InputDecoration(
-                      hintText: AppText.enterNameText,
-                      border: InputBorder.none,
-                      hintStyle: TextStyle(
-                        fontSize: 15,
-                        fontFamily: "LexendDecaRegularStyle",
-                      color: const Color(0xff6E6A7C).withOpacity(0.5),
-                      )
-                    ),
-                  ),
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.154,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const LinearGradient(
+                colors: [
+                  ColorApp.skyBlueColor,
+                  ColorApp.lightPurpleColor,
                 ],
-              ) ,
+              ),
             ),
-          ]
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.all(22),
+            width: MediaQuery.of(context).size.width - 50,
+            decoration: BoxDecoration(
+              color: ColorApp.whiteColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  AppText.yourNameText,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: ColorApp.slateGrayColor,
+                    fontFamily: "LexendDecaRegularStyle",
+                  ),
+                ),
+                TextFormField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                    hintText: AppText.enterNameText,
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(
+                      fontSize: 15,
+                      fontFamily: "LexendDecaRegularStyle",
+                      color: const Color(0xff6E6A7C).withOpacity(0.5),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
